790c5b10a6480f332bb94b0787a353a6325bad15
fatjon dungu <146867501+fatjond0413@users.noreply.github.com>
2023-11-28 18:48:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
 let userName = "YourUsername"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
+    e.innerHTML = fatjond0413;
 });
 
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
+document.querySelector("#github").href = "http://github.com/" + fatjond0413;
+document.querySelector("#fork").href = "http://github.com/" + fatjond0413 + "/" + fatjond0413 + ".github.io";
